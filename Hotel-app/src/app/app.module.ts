14d23c787ec2862da5b0eac78b6b2d74c47236ed
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2022-01-05 12:57:11 +0200

    building landing page

--- Hotel-app/src/app/app.module.ts
+++ Hotel-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RoomComponent } from './components/room/room.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RoomComponent
+    RoomComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
