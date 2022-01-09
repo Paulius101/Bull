a2aa40c6a0ce9d6faa9a9063f69d0d7b65013804
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2022-01-09 15:48:07 +0200

    navbar hamburger

--- Hotel-app/src/app/components/header/header.component.ts
+++ Hotel-app/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+public faBars = faBars;
+public state:number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  public onBurgerClick() {
+    this.state === 0 ? this.state = 1  : this.state = 0; 
+  }
 }
--- Hotel-app/src/app/components/landing-page/landing-page.component.ts
+++ Hotel-app/src/app/components/landing-page/landing-page.component.ts
@@ -33,13 +33,13 @@ export class LandingPageComponent implements OnInit {
 public onPreviousClick() {
   const previous = this.currentTestimonial - 1;
   this.currentTestimonial = previous < 0 ? this.testimonials.length - 1 : previous;
-    console.log("previous clicked, new current testimonial is: ", this.currentTestimonial);
+    // console.log("previous clicked, new current testimonial is: ", this.currentTestimonial);
 }
 
 public onNextClick() {
     const next = this.currentTestimonial + 1;
     this.currentTestimonial = next === this.testimonials.length ? 0 : next;
-    console.log("next clicked, new current testimonial is: ", this.currentTestimonial);
+    // console.log("next clicked, new current testimonial is: ", this.currentTestimonial);
   }
 
   ngOnInit(): void {
